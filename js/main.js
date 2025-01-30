b1832d0e421132b71383842d2c2af5415e10ef45
Programming <programming@Macintoshs-MacBook-Pro.local>
2025-01-29 19:01:29 -0500

    added removeClass after reauth is done or failed

--- js/main.js
+++ js/main.js
@@ -17,9 +17,6 @@ $(document).ready(() => {
     //     showLoginPage();
     // }
 
-    $('#navbar').removeClass('hide');
-    $('#body-container').removeClass('hide');
-
     reauth();
 
     $("#login-form").on("submit", (e) => {
