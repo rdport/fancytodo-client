06452e333773e30c397c901be621c06b7443b2a6
hactiv8rud <procord100@gmail.com>
2025-03-01 23:26:51 -0500

    edited helper.js

--- js/helper.js
+++ js/helper.js
@@ -1,5 +1,5 @@
-const baseURL = "http://localhost:3000";
-// const baseURL = "https://fancytodo-server.onrender.com";
+// const baseURL = "http://localhost:3000";
+const baseURL = "https://fancytodo-server.onrender.com";
 
 function showLoginPage() {
     $('#body-container').removeClass('hide');
